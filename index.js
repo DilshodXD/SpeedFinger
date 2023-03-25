7f0691668e63e164164e6fc3edfe527442c48353
Dilshod <dilshodxdxd>
2023-03-26 02:28:31 +0500

    Add js fn make random generation

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 let btns = document.querySelectorAll(".button");
 let text = document.querySelector(".text-inner");
 let lorem =
-  "alert arguments Array blur Boolean callee caller captureEvents clearInterval clearTimeout close closed confirm constructor Date defaultStatus document escape eval find focus frames Function history home Infinity innerHeight innerWidth isFinite isNaN java length location locationbar Math menubar moveBy name NaN netscape Number Object open opener outerHeight outerWidth Packages pageXOffset pageYOffset parent parseFloat parseInt personalbar print prompt prototype RegExp releaseEvents resizeBy resizeTo routeEvent scroll scrollbars scrollBy scrollTo self setInterval setTimeout status statusbar stop String toolbar top toString unescape unwatch valueOf watch window";
+  "alert  arguments Array blur Boolean callee caller captureEvents clearInterval clearTimeout close closed confirm constructor Date defaultStatus document escape eval find focus frames Function history home Infinity innerHeight innerWidth isFinite isNaN java length location locationbar Math menubar moveBy name NaN netscape Number Object open opener outerHeight outerWidth Packages pageXOffset pageYOffset parent parseFloat parseInt personalbar print prompt prototype RegExp releaseEvents resizeBy resizeTo routeEvent scroll scrollbars scrollBy scrollTo self setInterval setTimeout status statusbar stop String toolbar top toString unescape unwatch valueOf watch window abstract	arguments	await	boolean break	byte	case catch char class	const	continue debugger	default	delete	do double	else	enum	eval export	extends	false	final finally	float	for	function goto	if	implements	import in	instanceof	int	interface let	long	native	new null	package	private	protected public	return	short	static super	switch	synchronized	this throw	throws	transient	true try	typeof	var	void volatile	while	with	yield";
 
-let loremArr = lorem.split("");
+  // let loremArr = lorem.split("");
+let loremTextArr = lorem.split(" "); // sozlardan tashkil topgan Array  //  length = 80 
+let loremTextArrNew = []; // bosh Array. sozlar uchun yaratilgan Array
+
+for (let i = 0; loremTextArrNew.length != loremTextArr.length; i++) {
+  let randomNum = Math.floor(Math.random() * 1000); // 100 gacham bolgan sonlar
+
+  if (randomNum < loremTextArr.length) {
+        loremTextArrNew.push(loremTextArr[randomNum]);
+  }
+} // 
+
+let loremArr = loremTextArrNew.toString().replace(/,/g, " ").split(""); // harflar
 
 loremArr.map((item) => {
   let span = document.createElement("span");
@@ -11,11 +23,11 @@ loremArr.map((item) => {
   span.appendChild(text);
 
   document.querySelector(".text-inner").appendChild(span);
-});
+}); // har birini <span>  ichiga oladi
 
-let letter = text.querySelectorAll("span");
+let letter = text.querySelectorAll("span"); // har bir harf
 
-letter[0].style = "background-color: #495057;";
+letter[0].style = "background-color: #495057;"; // birinchi harf ortini boyaydi
 
 let i = 0;
 document.addEventListener("keydown", (e) => {
@@ -38,9 +50,7 @@ document.addEventListener("keydown", (e) => {
     if (letter.length > i) i++;
     letter[i].style = "background-color: #495057;";
   }
-  lgo;
-  console.log(i);
-});
+}); // harflar ortini bo'yaydi
 
 document.addEventListener("keydown", (e) => {
   let name = e.key;
@@ -55,4 +65,4 @@ document.addEventListener("keydown", (e) => {
       }, 100);
     }
   }
-});
+}); // klaviatura bosilganida bosilgan harfni korsatadi
